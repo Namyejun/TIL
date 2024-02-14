93790dc0ac13426ede02280db6c7b97172148256
Namyejun <yejun1148@gmail.com>
2024-02-14 23:50:13 +0900

    으악 어려워

--- CodingTest/Python/solution25.py
+++ CodingTest/Python/solution25.py
@@ -1,7 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/258707
 
 def solution(coin, cards):
-    answer = 0
+    answer = 1
     
     idx_of_cards = [i for i in range(len(cards))]
     pair_of_cards = [(len(cards) - i + 1) for i in cards]
@@ -17,6 +17,24 @@ def solution(coin, cards):
     print(idx_of_cards)
     print(idx_of_pairs)
 
-    deck = cards[:len(cards)//3]
+    card_set = set()
+    submit = 0
+    
+
+    for i in range(len(cards)):
+        if i < len(cards)//3:
+            if pair_of_cards[i] in card_set: # 같은 짝이 패에 있을 때 제출목록으로 넘김
+                card_set.pop(pair_of_cards[i])
+                submit += 1
+            else:
+                card_set.add(cards)
+        else:
+
+
+            if i % 2 == 1 and submit > 0:
+                submit -= 1
+                answer += 1
+            else:
+                break
 
     return answer
