a9f6b49b982767114290f7fcd420c309c931698b
Namyejun <yejun1148@gmail.com>
2024-02-19 00:09:19 +0900

    기록

--- CodingTest/Python/solution25.py
+++ CodingTest/Python/solution25.py
@@ -2,4 +2,12 @@
 
 def solution(coin, cards):
     answer = 0
+    A = cards[:len(cards)//3]
+    B = cards[len(cards)//3:]
+
+    while True:
+        # 가진 덱에 짤 수 있는 조합이 있는지 확인하고
+        # 없으면 A랑 B랑 조합해서 있는지 확인하고
+        # 그것 마저 없다면 B만 가지고 확인
+        pass
     return answer
