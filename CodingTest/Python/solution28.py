e59bad9243a7fac330b1b0f54e93c3f7e147b532
Namyejun <yejun1148@gmail.com>
2024-03-02 01:49:20 +0900

    어렵네;

--- CodingTest/Python/solution28.py
+++ CodingTest/Python/solution28.py
@@ -1,25 +1,46 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/118668
 
 def solution(alp, cop, problems):
-	answer = 0
 	maps = [[500 for _ in range(151)] for _ in range(151)]
-	maps[0][0] = 0
+	maps[alp][cop] = 0
 	plans = [[[(1, 0, 1), (0, 1, 1)] for _ in range(151)] for _ in range(151)]
 	for prob in problems:
 		for i in range(151):
 			for j in range(151):
-				if i > prob[0] and j >prob[1]:
+				if i > prob[0] and j > prob[1]:
 					plans[i][j].append((prob[2], prob[3], prob[4]))
-	
-	for x in range(301):
-		for i in range(x + 1):
-			if i > 151:
-				continue
-	
-			for j in range(x  + 1 - i):
-				if j > 151:
-					continue
-				maps[i][j] = min()
-					
 
+	from collections import deque
+	q = deque()
+	q.append([alp, cop])
+	mv = [[0, 1], [1, 0]]
+	while q:
+		x, y = q.popleft()
+		
+		for i, j in mv:
+			nx = x + i
+			ny = y + j
+			if alp <= nx <= 150 and cop <= ny <= 150 and maps[nx][ny] == 500:
+				lst = []
+				for plan in plans[nx][ny]:
+					lst.append(maps[nx - plan[0]][ny - plan[1]] + plan[2])
+				maps[nx][ny] = min(lst)
+				q.append([nx, ny])
+
+	max_a = 0
+	max_c = 0
+	for a, c, _, _, _ in problems:
+		if max_a < a:
+			max_a = a
+		if max_c < c:
+			max_c = c
+	
+	answer = 999
+	for i in range(max_a, 151):
+		for j in range(max_c, 151):
+			if answer > maps[i][j]:
+				answer = maps[i][j]
+	print(maps[10][11])
 	return answer
+
+# 문제가 뭐냐면 이게 plans를 초기화 할 때 그냥 
