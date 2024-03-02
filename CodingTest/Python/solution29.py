dccb4a4eb4be4dde3ef9898792f0a4773acc5034
Namyejun <yejun1148@gmail.com>
2024-03-03 01:57:00 +0900

    등산코스 정하기

--- CodingTest/Python/solution29.py
+++ CodingTest/Python/solution29.py
@@ -2,4 +2,14 @@
 
 def solution(n, paths, gates, summits):
     answer = []
+    graph = [[0 for j in range(n + 1)] for i in range(n + 1)]
+    for i, j, w in paths:
+        graph[i][j] = w
+        graph[j][i] = w
+    
+    for summit in summits:
+        pass
     return answer
+
+# dp로 안되나??
+# 2차원 배열로
