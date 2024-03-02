9c876c82709d1a4d4825d2d6054c1e60161ba794
Namyejun <yejun1148@gmail.com>
2024-03-02 17:36:39 +0900

    코딩 테스트 공부 : 이런건 힘든데

--- CodingTest/Python/solution28.py
+++ CodingTest/Python/solution28.py
@@ -1,45 +1,30 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/118668
 
 def solution(alp, cop, problems):
-	maps = [[500 for _ in range(151)] for _ in range(151)]
-	maps[alp][cop] = 0
-	plans = [[[(1, 0, 1), (0, 1, 1)] for _ in range(151)] for _ in range(151)]
-	for prob in problems:
-		for i in range(151):
-			for j in range(151):
-				if i >= prob[0] and j > prob[1] or i > prob[0] and j >= prob[1]:
-					plans[i][j].append((prob[2], prob[3], prob[4]))
-
-	from collections import deque
-	q = deque()
-	q.append([alp, cop])
-	mv = [[0, 1], [1, 0]]
-	while q:
-		x, y = q.popleft()
-		
-		for i, j in mv:
-			nx = x + i
-			ny = y + j
-			if alp <= nx <= 150 and cop <= ny <= 150 and maps[nx][ny] == 500:
-				lst = []
-				for plan in plans[nx][ny]:
-					lst.append(maps[nx - plan[0]][ny - plan[1]] + plan[2])
-				maps[nx][ny] = min(lst)
-				q.append([nx, ny])
-
 	max_a = 0
 	max_c = 0
-	for a, c, _, _, _ in problems:
-		if max_a < a:
-			max_a = a
-		if max_c < c:
-			max_c = c
+	for problem in problems:
+		max_a = max(max_a, problem[0])
+		max_c = max(max_c, problem[1])
+	dp = [[9999 for _ in range(max_c + 1)] for _ in range(max_a + 1)]
 	
-	answer = 999
-	for i in range(max_a, 151):
-		for j in range(max_c, 151):
-			if answer > maps[i][j]:
-				answer = maps[i][j]
-	return answer
+	alp = min(alp, max_a)
+	cop = min(cop, max_c)
+	dp[alp][cop] = 0
+	for i in range(alp, max_a + 1):
+		for j in range(cop, max_c + 1):
+			# 문제 풀이 없이 공부하는 방법
+			if i < max_a:
+				dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1)
+			if j < max_c:
+				dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
+			# 문제 풀이로 공부하는 방법
+			for problem in problems:
+				if i >= problem[0] and j >= problem[1]:
+					a = min(i + problem[2], max_a)
+					c = min(j + problem[3], max_c)
+					dp[a][c] = min(dp[a][c], dp[i][j] + problem[-1])
+					
+	return dp[max_a][max_c]
 
-# 문제가 뭐냐면 이게 plans를 초기화 할 때 그냥 
+# BFS로 하면 효율성 박살난대
