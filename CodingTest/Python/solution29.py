387d9354f0517670a76311bf7d6de4be21c14303
Namyejun <yejun1148@gmail.com>
2024-03-04 00:55:11 +0900

    등산코스 정하기 : 기본 다익스트라까지만 구현

--- CodingTest/Python/solution29.py
+++ CodingTest/Python/solution29.py
@@ -6,10 +6,32 @@ def solution(n, paths, gates, summits):
     for i, j, w in paths:
         graph[i][j] = w
         graph[j][i] = w
-    
-    for summit in summits:
-        pass
-    return answer
 
-# dp로 안되나??
-# 2차원 배열로
+
+    for gate in gates:
+        dist = ["inf" for i in range(n + 1)]
+        visit = [0 for i in range(n + 1)]
+        dist[gate] = 0
+
+        for i in range(1, len(dist)):
+            min_node = 0
+            min_value = 99999999
+            for j in range(1, len(dist)): # 최소 노드 찾기
+                if visit[j] == 0 and dist[j] != "inf":
+                    if min_value > dist[j]:
+                        min_value = dist[j]
+                        min_node = j
+            visit[min_node] = 1
+
+            for j in range(1, len(graph[min_node])):
+                if graph[min_node][j] != 0 and dist[j] == "inf":
+                    dist[j] = dist[min_node] + graph[min_node][j]
+                elif graph[min_node][j] != 0 and dist[j] != "inf":
+                    dist[j] = min(dist[j], dist[min_node] + graph[min_node][j])
+
+        print(dist)
+            
+
+
+        
+    return answer
