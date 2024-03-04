ad194305c7e0e36ce1de21f7e564b36728328fdb
Namyejun <yejun1148@gmail.com>
2024-03-04 23:44:52 +0900

    등산코스 정하기

--- CodingTest/Python/solution29.py
+++ CodingTest/Python/solution29.py
@@ -1,37 +1,42 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/118669
-
+import heapq
+import math
 def solution(n, paths, gates, summits):
-    answer = []
-    graph = [[0 for j in range(n + 1)] for i in range(n + 1)]
-    for i, j, w in paths:
-        graph[i][j] = w
-        graph[j][i] = w
+    inf = math.inf
+    
+    graph = [[] for i in range(n + 1)]
+    for path in paths:
+        graph[path[0]].append([path[1], path[2]])
+        graph[path[1]].append([path[0], path[2]])
 
-
+    is_summit = [False] * (n + 1)
+    for summit in summits:
+        is_summit[summit] = True
+    
+    dist = [inf for i in range(n + 1)]
+    q = []
     for gate in gates:
-        dist = ["inf" for i in range(n + 1)]
-        visit = [0 for i in range(n + 1)]
         dist[gate] = 0
+        heapq.heappush(q, [0, gate])
 
-        for i in range(1, len(dist)):
-            min_node = 0
-            min_value = 99999999
-            for j in range(1, len(dist)): # 최소 노드 찾기
-                if visit[j] == 0 and dist[j] != "inf":
-                    if min_value > dist[j]:
-                        min_value = dist[j]
-                        min_node = j
-            visit[min_node] = 1
+    while q:
+        d, i = heapq.heappop(q)
+        if dist[i] < d or is_summit[i]:
+            continue
+        for j, new_d in graph[i]:
+            new_d = max(dist[i], new_d)
+            if dist[j] > new_d:
+                dist[j] = new_d
+                heapq.heappush(q, [new_d, j])
 
-            for j in range(1, len(graph[min_node])):
-                if graph[min_node][j] != 0 and dist[j] == "inf":
-                    dist[j] = dist[min_node] + graph[min_node][j]
-                elif graph[min_node][j] != 0 and dist[j] != "inf":
-                    dist[j] = min(dist[j], dist[min_node] + graph[min_node][j])
+    answer = [0, inf]
+    for summit in sorted(summits):
+        if dist[summit] < answer[1]:
+            answer[0] = summit
+            answer[1] = dist[summit]
 
-        print(dist)
-            
-
-
-        
     return answer
+# ['inf', 0, 3, 4, 3, 3, 3]
+# ['inf', 4, 4, 0, 4, 4, 4]
+
+#[[1, 2, 3], [2, 3, 5], [2, 4, 2], [2, 5, 4], [3, 4, 4], [4, 5, 3], [4, 6, 1], [5, 6, 1]]	[1, 3]	[5]	[5, 3]
