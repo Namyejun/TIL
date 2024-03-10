41608810e381c47194fc5e9e06972b089bdfee48
Namyejun <yejun1148@gmail.com>
2024-03-10 15:00:50 +0900

    신고 결과 받기

--- CodingTest/Python/main.py
+++ CodingTest/Python/main.py
@@ -1,7 +1,5 @@
-from solution30 import solution
+from solution31 import solution
 
 if __name__ == "__main__":
-    print(solution([[1, 2, 3], [4, 5, 6], [7, 8, 9]], ["Rotate", "ShiftRow"]))
-    # print(solution([[8, 6, 3], [3, 3, 7], [8, 4, 9]], ["Rotate", "ShiftRow", "ShiftRow"]))
-    # print(solution([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], ["ShiftRow", "Rotate", "ShiftRow", "Rotate"]))
-    # print(solution([[1, 2, 3], [4, 5, 6], [7, 8, 9]], ["Rotate", "Rotate", "Rotate", "Rotate", "Rotate", "ShiftRow"]))
+    print(solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],2), [2,1,1,0])
+    print(solution(["con", "ryan"],["ryan con", "ryan con", "ryan con", "ryan con"],3),[0,0])
