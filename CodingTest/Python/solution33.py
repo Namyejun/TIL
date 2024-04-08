2efbacc4e6e679427becdb2fdd542dad02d4824b
Namyejun <yejun1148@gmail.com>
2024-04-08 20:57:39 +0900

    양궁대회 : 시험으로 인해 늦어졌다...

--- CodingTest/Python/solution33.py
+++ CodingTest/Python/solution33.py
@@ -1,7 +1,63 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/92342#qna
+# https://school.programmers.co.kr/questions/25527
+
+
+# import sys
+# sys.setrecursionlimit(10**7)
+
+ryon_lst = []
+
 def solution(n, info):
     answer = []
+    min_inv = [info[i] + 1 for i in range(11)]
+    lst = [0 for i in range(11)]
+    dfs(0, n, lst, min_inv)
+    result = []
+    
+    for r in ryon_lst:
+        result.append([calc(info, r), r])
+    A = sorted(result, reverse = True)
+    if A[0][0] > 0:
+        t = 0
+        max_lst = []
+        while A[t][0] == A[0][0]:
+            min_point = -1
+            for i in reversed(range(11)):
+                if A[t][1][i] != 0:
+                    min_point = (10 - i)
+                    break
+            max_lst.append([min_point, A[t][1]])
+            t += 1
+        
+        B = sorted(max_lst)
+        if sum(B[0][1]) != n:
+            B[0][1][-1] += n - sum(B[0][1])
+        return B[0][1]
+    else:
+        return [-1]
+        
     return answer
+    
+def dfs(i, k, lst, min_inv):
+    if i == 11 or k == 0:
+        ryon_lst.append(lst)
+        return
+    new_lst = lst[:]
+    dfs(i + 1, k, new_lst, min_inv)
+    if k - min_inv[i] >= 0:
+        new_lst[i] += min_inv[i]
+        dfs(i + 1, k - min_inv[i], new_lst, min_inv)
 
-#
-# 그냥 모든 점수들에 대해서 다 이기도록 해놓고 뺄까
-#
+def calc(apeach, ryon):
+    a = 0
+    r = 0
+    for i in range(11):
+        if apeach[i] == 0 and ryon[i] == 0:
+            continue
+        
+        if apeach[i] >= ryon[i]:
+            a += (10 - i)
+        else:
+            r += (10 - i)
+    
+    return r - a
