aab00acb11670943feac874376616a735d35b79d
Namyejun <yejun1148@gmail.com>
2024-04-11 20:13:01 +0900

    양과 늑대

--- CodingTest/Python/solution34.py
+++ CodingTest/Python/solution34.py
@@ -2,4 +2,16 @@
 
 def solution(info, edges):
     answer = 0
+
+    tree = [[] for i in range(len(info))]
+    for i, j in edges:
+        tree[i].append(j)
+
     return answer
+
+def dfs(tree, explorerable, lst):
+    for i in explorerable:
+        new_lst = lst[:]
+        new_lst.append(i)
+        new_exp = explorerable[:]
+        new_exp += tree[i]
