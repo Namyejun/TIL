0a3b0b207cbec65b29dd65a0972c49ad7cf276bf
Namyejun <yejun1148@gmail.com>
2024-04-14 19:14:22 +0900

    양과 늑대

--- CodingTest/Python/solution34.py
+++ CodingTest/Python/solution34.py
@@ -1,28 +1,23 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/92343
-result = []
+
 def solution(info, edges):
-    answer = 0
-
-    tree = [[] for i in range(len(info))]
-    for i, j in edges:
-        tree[i].append(j)
-
-    dfs(info, tree, [0],[],0,0)
-
-    return max(result)
-
-def dfs(info, tree, explorerable, lst, s, w):
-    if s <= w or not explorerable:
-        result.append(s)
-        return
-    for i in explorerable:
-        if i >= len(info):
-            continue
-        new_lst = lst[:]
-        new_lst.append(i)
-        new_exp = explorerable[:]
-        new_exp += tree[i]
-        if info[i] == 0:
-            dfs(info, tree, new_exp, new_lst, s + 1, w)
+    answer = []
+    visit = [0 for i in range(len(info))]
+    def dfs(s, w):
+        if s > w:
+            answer.append(s)
         else:
-            dfs(info, tree, new_exp, new_lst, s, w + 1)
+            return
+        
+        for i, j in edges:
+            if visit[i] == 1 and visit[j] == 0:
+                visit[j] = 1
+                if info[j] == 0:
+                    dfs(s + 1, w)
+                else:
+                    dfs(s, w + 1)
+                visit[j] = 0
+    visit[0] = 1
+    dfs(1, 0)
+    
+    return max(answer)
