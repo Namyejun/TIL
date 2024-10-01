4b61f7000f2d36faee4dbbd9a91064e3bef95983
Namyejun <yejun1148@gmail.com>
2024-10-01 18:15:35 +0900

    충돌위험 찾기

--- CodingTest/Python/solution39.py
+++ CodingTest/Python/solution39.py
@@ -10,8 +10,11 @@ def solution(points, routes):
     # 다음에 향할 곳은 포인트 도착 시 route의 앞 부분을 pop하는 방식
     for i in range(len(routes)):
         robots.append([points[routes[i][0]], routes[i][1:], 0])
-    
-    while chk_finished(robots):
+
+    answer += chk_crash(robots)
+    while True:
+        if chk_finished(robots):
+            break
         move(robots, points)
         answer += chk_crash(robots)
     
@@ -23,12 +26,12 @@ def solution(points, routes):
 def move(robots, points):
     for i in range(len(robots)):
         robot = robots[i]
-
         state = robot[2]
-        cur_r, cur_c = robot[0][0], robot[0][1]
-        dest_r, dest_c = points[robot[1][0]][0], points[robot[1][0]][0][1]
 
-        if state != 0:
+        if state != 1:
+            cur_r, cur_c = robot[0][0], robot[0][1]
+            dest_r, dest_c = points[robot[1][0]][0], points[robot[1][0]][1]
+        
             if dest_r - cur_r < 0:
                 cur_r -= 1
             elif dest_r - cur_r > 0:
@@ -38,10 +41,12 @@ def move(robots, points):
                     cur_c -= 1
                 elif dest_c - cur_c > 0:
                     cur_c += 1
-                else:
-                    state = 1
-        
-        robots[i] = [[cur_r, cur_c], robot[1][1:], state]
+
+            if dest_r == cur_r and dest_c == cur_c:
+                robots[i] = [[cur_r, cur_c], robot[1][1:], state]
+            else:
+                robots[i] = [[cur_r, cur_c], robot[1], state]
+
 
 # 끝났는지 확인
 def chk_finished(robots):
@@ -56,20 +61,22 @@ def chk_finished(robots):
 
 # 충돌 체크 메서드
 def chk_crash(robots):
-    # 숫자 세기
     cnt = 0
     # 충돌 위치 체크
     crash_dic = dict()
 
-    for robot in robots:
+    for i in range(len(robots)):
+        robot = robots[i]
         # 1은 이미 도착해서 사라진 로봇이라 뺌
         if robot[2] != 1:
-            if crash_dic.get(robot[0]) != None:
-                crash_dic[robot[0]] = 1
+            if str(robot[0][0]) + str(robot[0][1]) not in crash_dic:
+                crash_dic[str(robot[0][0]) + str(robot[0][1])] = 1
             else:
-                crash_dic[robot[0]] += 1
-    
-    return len(crash_dic.keys())
+                crash_dic[str(robot[0][0]) + str(robot[0][1])] += 1
+            if len(robot[1]) == 0:
+                robots[i][2] = 1
 
-
-# main    
+    for i in crash_dic.values():
+        if i >= 2:
+            cnt += 1
+    return cnt
