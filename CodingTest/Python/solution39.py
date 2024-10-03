4e2002f21c9464868458d10129b03bc0fbbf0bec
Namyejun <yejun1148@gmail.com>
2024-10-04 00:07:26 +0900

    충돌위험 찾기

--- CodingTest/Python/solution39.py
+++ CodingTest/Python/solution39.py
@@ -1,84 +1,37 @@
 def solution(points, routes):
     answer = 0
-    # 포인트의 시작 번호가 1번부터 시작되므로  0번 인덱스 추가
-    points = [[]] + points
     
-    # 로봇이 기억할 것은 본인의 현재 위치와 다음에 향할 곳
+    points = [[]] + points
     robots = []
 
-    # 로봇s 초기화
-    # 다음에 향할 곳은 포인트 도착 시 route의 앞 부분을 pop하는 방식
+    # 로봇들 움직이는 루트 기록
     for i in range(len(routes)):
-        robots.append([points[routes[i][0]], routes[i][1:], 0])
+        robot = []
+        
+        sx, sy = points[routes[i][0]]
+        robot.append([sx, sy])
+        
+        for j in range(1, len(routes[i])):
+            ex, ey = points[routes[i][j]]
+            
+            while ex != sx:
+                if ex - sx > 0:
+                    sx += 1
+                else:
+                    sx -= 1
+                robot.append([sx, sy])
+            while ey != sy:
+                if ey - sy > 0:
+                    sy += 1
+                else:
+                    sy -= 1
+                robot.append([sx, sy])
 
-    answer += chk_crash(robots)
-    while True:
-        if chk_finished(robots):
-            break
-        move(robots, points)
-        answer += chk_crash(robots)
-        print(robots)
-        print(answer)
-    
+        robots.append(robot)
+
+    # 일단 로봇마다의 경로는 구해놓음. 이제 시간에 따라서 충돌 구하면 됨
+    print(robots)
+        
     return answer
     
-# 로봇 움직이는 메서드와 모든 로봇이 움직인 후 충돌 개수 확인하는 메서드 두 개 필요
-
-#로봇 움직이는 메서드로 아예 로봇 리스트와 어떤 로봇인지 받아옴
-def move(robots, points):
-    for i in range(len(robots)):
-        robot = robots[i]
-        state = robot[2]
-
-        if state != 1:
-            cur_r, cur_c = robot[0][0], robot[0][1]
-            dest_r, dest_c = points[robot[1][0]][0], points[robot[1][0]][1]
-        
-            if dest_r - cur_r < 0:
-                cur_r -= 1
-            elif dest_r - cur_r > 0:
-                cur_r += 1
-            else:
-                if dest_c - cur_c < 0:
-                    cur_c -= 1
-                elif dest_c - cur_c > 0:
-                    cur_c += 1
-
-            if dest_r == cur_r and dest_c == cur_c:
-                robots[i] = [[cur_r, cur_c], robot[1][1:], state]
-            else:
-                robots[i] = [[cur_r, cur_c], robot[1], state]
-
-
-# 끝났는지 확인
-def chk_finished(robots):
-    chk = 1
-    for robot in robots:
-        chk *= robot[2]
-    
-    if chk:
-        return True
-    else:
-        return False
-
-# 충돌 체크 메서드
-def chk_crash(robots):
-    cnt = 0
-    # 충돌 위치 체크
-    crash_dic = dict()
-
-    for i in range(len(robots)):
-        robot = robots[i]
-        # 1은 이미 도착해서 사라진 로봇이라 뺌
-        if robot[2] != 1:
-            if str(robot[0][0]) + str(robot[0][1]) not in crash_dic:
-                crash_dic[str(robot[0][0]) + str(robot[0][1])] = 1
-            else:
-                crash_dic[str(robot[0][0]) + str(robot[0][1])] += 1
-            if len(robot[1]) == 0:
-                robots[i][2] = 1
-
-    for i in crash_dic.values():
-        if i >= 2:
-            cnt += 1
-    return cnt
+# 뭐가 틀렸을까요...
